a8054cbda802b8031e90214e5e13e538a7a4e520
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2022-05-09 22:29:38 +0200

    style jeu - work in progress

--- IUT-Demineur-Project/accueil.Designer.vb
+++ IUT-Demineur-Project/accueil.Designer.vb
@@ -32,52 +32,66 @@ Partial Class accueil
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(51, 141)
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.Location = New System.Drawing.Point(51, 328)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(195, 50)
-        Me.Button1.TabIndex = 16
+        Me.Button1.TabIndex = 1
         Me.Button1.Text = "Nouvelle partie"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(303, 141)
+        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button2.Location = New System.Drawing.Point(303, 328)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(195, 50)
-        Me.Button2.TabIndex = 17
+        Me.Button2.TabIndex = 2
         Me.Button2.Text = "Scores"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.UseVisualStyleBackColor = False
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(554, 141)
+        Me.Button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button3.Location = New System.Drawing.Point(554, 328)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(195, 50)
-        Me.Button3.TabIndex = 18
+        Me.Button3.TabIndex = 3
         Me.Button3.Text = "Quitter"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.UseVisualStyleBackColor = False
         '
         'ComboBox1
         '
         Me.ComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
         Me.ComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(235, 63)
+        Me.ComboBox1.Location = New System.Drawing.Point(189, 191)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(231, 21)
-        Me.ComboBox1.TabIndex = 19
+        Me.ComboBox1.Size = New System.Drawing.Size(417, 37)
+        Me.ComboBox1.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(158, 66)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(116, 198)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.Size = New System.Drawing.Size(56, 25)
         Me.Label1.TabIndex = 20
         Me.Label1.Text = "Nom"
         '
@@ -85,10 +99,11 @@ Partial Class accueil
         '
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.SystemColors.Window
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Red
-        Me.Label2.Location = New System.Drawing.Point(240, 67)
+        Me.Label2.Location = New System.Drawing.Point(190, 173)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(110, 13)
+        Me.Label2.Size = New System.Drawing.Size(165, 20)
         Me.Label2.TabIndex = 22
         Me.Label2.Text = "Le nom est obligatoire"
         Me.Label2.Visible = False
@@ -97,10 +112,11 @@ Partial Class accueil
         '
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.Red
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(48, 20)
+        Me.Label3.Location = New System.Drawing.Point(26, 270)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(514, 13)
+        Me.Label3.Size = New System.Drawing.Size(762, 20)
         Me.Label3.TabIndex = 23
         Me.Label3.Text = "L'application n'a pas trouvé le fichier de configuration. Veuillez changer le che" &
     "min d'accès dans les réglages"
@@ -110,29 +126,45 @@ Partial Class accueil
         '
         Me.Label4.AutoSize = True
         Me.Label4.BackColor = System.Drawing.SystemColors.Window
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.Red
-        Me.Label4.Location = New System.Drawing.Point(240, 67)
+        Me.Label4.Location = New System.Drawing.Point(199, 200)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(191, 13)
+        Me.Label4.Size = New System.Drawing.Size(289, 20)
         Me.Label4.TabIndex = 24
         Me.Label4.Text = "Le nom doit être supérieur à trois lettres"
         Me.Label4.Visible = False
         '
         'PictureBox1
         '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.PictureBox1.Image = Global.IUT_Demineur_Project.My.Resources.Resources.reglage
-        Me.PictureBox1.Location = New System.Drawing.Point(554, 49)
+        Me.PictureBox1.Location = New System.Drawing.Point(639, 185)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
         Me.PictureBox1.TabIndex = 21
         Me.PictureBox1.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Showcard Gothic", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label5.Location = New System.Drawing.Point(22, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(333, 60)
+        Me.Label5.TabIndex = 25
+        Me.Label5.Text = "Le démineur"
         '
         'accueil
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(800, 231)
+        Me.BackgroundImage = Global.IUT_Demineur_Project.My.Resources.Resources.fond
+        Me.ClientSize = New System.Drawing.Size(800, 444)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -143,7 +175,6 @@ Partial Class accueil
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.ImeMode = System.Windows.Forms.ImeMode.NoControl
         Me.MaximizeBox = False
         Me.Name = "accueil"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -163,4 +194,5 @@ Partial Class accueil
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
